db8996e44906fb274f42f231c464b4a115571caa
mvimal2607 <mvimal2607@gmail.com>
2025-05-23 07:37:05 +0000

    sepolicy: Change libperfmgr for yaap
    Signed-off-by: mvimal2607 <mvimal2607@gmail.com>

--- sepolicy/qti/SEPolicy.mk
+++ sepolicy/qti/SEPolicy.mk
@@ -29,4 +29,4 @@ SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     hardware/oplus/sepolicy/qti/public/common-um
 endif
 
-include device/calyx/sepolicy/libperfmgr/sepolicy.mk
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
